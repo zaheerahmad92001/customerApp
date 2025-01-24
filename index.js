dadd2f6199caba456542c58cf1590aecfd630f1f
zaheerahmad92001 <zaheerahmad92001@gmail.com>
2025-01-25 03:36:28 +0500

    Changes

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import InvoicesDetail from './src/screens/invoiceScreen/allInovicesDetails/invoicesDetail';
+import Profile from './src/screens/profileScreen/profile/profile';
 
-AppRegistry.registerComponent(appName, () => InvoicesDetail);
+AppRegistry.registerComponent(appName, () => Profile);
