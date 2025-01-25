7cc0e4d34588bee2c33670075fb34d0af6c77d87
zaheerahmad92001 <zaheerahmad92001@gmail.com>
2025-01-26 00:17:56 +0500

    Changes

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import TermCondition from './src/screens/termConditionScreen/termCondition/termCondition';
+import LoyaltyPoints from './src/screens/loyaltyPointsScreen/loyaltyPoints/loyaltyPoints';
 
-AppRegistry.registerComponent(appName, () => TermCondition);
+AppRegistry.registerComponent(appName, () => LoyaltyPoints);
