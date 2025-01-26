229a3ba9f7f3bf73f8bb0310b517cbbf9be4d94f
zaheerahmad92001 <zaheerahmad92001@gmail.com>
2025-01-26 16:54:52 +0500

    Nail Art

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Spa from './src/screens/spaScreen/spa/spa';
+import NailArt from './src/screens/nailArtScreen/nailArt/nailArt';
 
-AppRegistry.registerComponent(appName, () => Spa);
+AppRegistry.registerComponent(appName, () => NailArt);
