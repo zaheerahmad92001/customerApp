4bbabc918a00f732c5f30e410a87e2893e54c12c
zaheerahmad92001 <zaheerahmad92001@gmail.com>
2025-01-26 16:52:11 +0500

    Spa Screen

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Salon from './src/screens/salonScreen/salon/salon';
+import Spa from './src/screens/spaScreen/spa/spa';
 
-AppRegistry.registerComponent(appName, () => Salon);
+AppRegistry.registerComponent(appName, () => Spa);
