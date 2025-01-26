abadb8906681ebb24c9d21bf09cc30ee35f8032c
zaheerahmad92001 <zaheerahmad92001@gmail.com>
2025-01-26 22:31:43 +0500

    Booked Successfuly

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import PaymentMethod from './src/screens/paymentMethodScreen/paymentMethod/paymentMethod';
+import BookedSuccess from './src/screens/bookedSuccessScreen/bookedSuccess/bookedSuccess';
 
-AppRegistry.registerComponent(appName, () => PaymentMethod);
+AppRegistry.registerComponent(appName, () => BookedSuccess);
