e138734e0a9bcdfe31b11a9bedba435848d5253e
zaheerahmad92001 <zaheerahmad92001@gmail.com>
2025-01-29 00:28:14 +0500

    Changes

--- index.js
+++ index.js
@@ -5,6 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Favorites from './src/screens/favoritesScreen/favourites';
 
-AppRegistry.registerComponent(appName, () => Favorites);
+AppRegistry.registerComponent(appName, () => App);
