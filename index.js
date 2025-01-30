188800a84969761bf0354b381a970e6ecb6b20e3
zaheerahmad92001 <zaheerahmad92001@gmail.com>
2025-01-30 21:47:11 +0500

    Register Successfuly

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import LanguageLogin from './src/screens/loginScreen/login/login';
+import RegisteredSuccessfuly from './src/screens/registeredSuccesfulyScreen/registeredSuccesfuly/RegisteredSuccesfuly';
 
-AppRegistry.registerComponent(appName, () => LanguageLogin);
+AppRegistry.registerComponent(appName, () => RegisteredSuccessfuly);
