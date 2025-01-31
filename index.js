96faa4703a0cf21410a7c8fe14c28920f4a6c510
zaheerahmad92001 <zaheerahmad92001@gmail.com>
2025-02-01 01:21:22 +0500

    SignUp

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ import 'react-native-get-random-values';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import OtpView from './src/screens/otpScreen/otp/otp';
+import SignUp from './src/screens/signUpScreen/signup/signUp';
 
-AppRegistry.registerComponent(appName, () => OtpView);
+AppRegistry.registerComponent(appName, () => SignUp);
