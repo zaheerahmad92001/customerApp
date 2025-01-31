f738acf20990b1e1fcbb8300dff90e484251308d
zaheerahmad92001 <zaheerahmad92001@gmail.com>
2025-02-01 01:25:26 +0500

    Changes

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ import 'react-native-get-random-values';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import SignUp from './src/screens/signUpScreen/signup/signUp';
 
-AppRegistry.registerComponent(appName, () => SignUp);
+AppRegistry.registerComponent(appName, () => App);
